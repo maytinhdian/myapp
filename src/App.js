94f0af3e251954b172629c0dfcd0946efcd3f46a
Lê Thanh Nhã <tnhalk@maytinhdian.com>
2023-11-18 14:10:55 +0700

    First Install React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -15,9 +15,12 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World
         </a>
       </header>
+      <footer className="App-footer">
+        <h3>TMT Innovative Sollution</h3>
+      </footer>
     </div>
   );
 }
