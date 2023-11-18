26c045f58f7df0a496127fb631a290a550d52b49
Lê Thanh Nhã <tnhalk@maytinhdian.com>
2023-11-18 17:21:31 +0700

    Sử dụng state trong component

--- src/components/CounterComponent.jsx
+++ src/components/CounterComponent.jsx
@@ -8,14 +8,22 @@ export default class CounterComponent extends Component {
     //Initial state or declare a state
     this.state = {
       count: 0,
+      isLoading: false,
     };
+  }
+  handleUpdateCount=()=>{
+    console.log("State of count: "+this.state.count + " is loading: "+this.state.isLoading);
+    this.setState({count: this.state.count+1});
+    // Không được update giá trị trực tiếp như sau : 
+    // this.setState.count = this.state.count + 1 ;
+
   }
   render() {
     return (
       <div>
         {/* Reading state  */}
         <p>You click {this.state.count} time</p>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+        <button onClick={() => this.handleUpdateCount()}>
           Click me
         </button>
       </div>
